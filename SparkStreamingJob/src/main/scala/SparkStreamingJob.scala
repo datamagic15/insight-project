2815d65cad808dedac265ac753815842bfc0c88b
AR-code <AR-code-99@protonmail.com>
2020-02-04 09:52:54 -0800

    Attempting to user event date in Spark Job

--- SparkStreamingJob/src/main/scala/SparkStreamingJob.scala
+++ SparkStreamingJob/src/main/scala/SparkStreamingJob.scala
@@ -21,30 +21,58 @@ object SparkStreamingJob {
     val lines = spark.readStream.
       format("kafka").
       option("kafka.bootstrap.servers", conf.getString("bootstrap.servers")).
-      option("subscribe", "Kafka-Testing").
+      option("subscribe", "edgar-logs").
       option("includeTimestamp", true).
       load()
 
     val kafkaLines = lines.selectExpr("CAST(value AS STRING)", "timestamp").
       as[(String, Timestamp)]
+    val logsDF = kafkaLines.where(split($"value", ",")(0) =!= "ip").
+      select(
+        split($"value", ",")(0).alias("IP"),
+        split($"value", ",")(1).alias("DATE"),
+        split($"value", ",")(2).alias("TIME"),
+        concat(split($"value", ",")(1),
+          lit(" "),
+          split($"value", ",")(2)
+        ).alias("TIMESTAMP"),
+        split($"value", ",")(4).alias("CIK"),
+        split($"value", ",")(4).alias("ACCESSION"),
+        split($"value", ",")(4).alias("DOC"),
+        split($"value", ",")(4).alias("CODE"),
+        split($"value", ",")(4).alias("SIZE"),
+        split($"value", ",")(4).alias("IDX"),
+        split($"value", ",")(4).alias("NOREFER"),
+        split($"value", ",")(4).alias("NOAGENT"),
+        split($"value", ",")(4).alias("FIND"),
+        split($"value", ",")(4).alias("CRAWLER")
+      )
 
+    val countsDF = logsDF.select($"TIMESTAMP", $"CIK").
+      groupBy(
+        window($"TIMESTAMP", "300 seconds", "30 seconds"), $"CIK"
+      ).count()
+
+    /*
     val logsDF = kafkaLines.where(split($"value", ",")(0) =!= "ip").
       select(split($"value", ",")(4).alias("CIK"), $"timestamp").
       groupBy(
-        window($"timestamp", "30 seconds", "30 seconds"), $"CIK"
+        window($"timestamp", "20 seconds", "20 seconds"), $"CIK"
       ).
       count()
-
+*/
       //.orderBy(desc("count"))
 
 
 
-    val query = logsDF.
+    val query = countsDF.
       writeStream.
       outputMode("update").
       format("console").
       trigger(Trigger.ProcessingTime("30 seconds")).
       start()
+
+
     /*
     val kafkaLines = lines.selectExpr("CAST(value AS STRING)", "timestamp").
       as[(String, Timestamp)]
