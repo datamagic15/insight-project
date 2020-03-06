35e5c5aa663da50a6663d99b7d6e3c0a2fbb5ee5
AR-code <AR-code-99@protonmail.com>
2020-03-06 15:14:58 -0800

    Using ENV variables instead of application.properties

--- SparkStreamingJob/build.sbt
+++ SparkStreamingJob/build.sbt
@@ -4,7 +4,6 @@ version := "1.0"
 
 scalaVersion := "2.11.12"
 
-libraryDependencies += "com.typesafe" % "config" % "1.3.2"
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "1.0.0"
 libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.4.0"
 libraryDependencies += "org.apache.spark" % "spark-sql-kafka-0-10_2.11" % "2.4.0"
--- SparkStreamingJob/src/main/scala/SparkStreamingJob.scala
+++ SparkStreamingJob/src/main/scala/SparkStreamingJob.scala
@@ -1,11 +1,6 @@
-import java.sql.Timestamp
-
-import com.typesafe.config.ConfigFactory
-import org.apache.spark.sql.{DataFrame, ForeachWriter, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger
-import java.sql.DriverManager
-import java.sql.Connection
 import java.util.Properties
 
 object SparkStreamingJob {
@@ -27,7 +22,6 @@ object SparkStreamingJob {
 
     Class.forName(driver)
 
-    val conf = ConfigFactory.load.getConfig(args(0))
     val spark_master_node = sys.env("SPARK_MASTER")
     val hdfs_path = "hdfs://" + spark_master_node + ":9000"
     val spark_path = "spark://" + spark_master_node + ":7077"
@@ -48,7 +42,6 @@ object SparkStreamingJob {
       format("kafka").
       option("kafka.bootstrap.servers", bootstrap_server ).
       option("subscribe", "edgar-logs").
-      //option("includeTimestamp", true).
       load()
 
     val kafkaLines = lines.selectExpr("CAST(value AS STRING)").
@@ -116,63 +109,13 @@ object SparkStreamingJob {
       staticSumsDF.col("avg_cnt").multiply(4).alias("big_cnt")
     ).filter( col("stream_count") > col("big_cnt") )
 
-    /*
-    val query = joinStaticAndStream.writeStream
-      .format("console")
-      .trigger(Trigger.ProcessingTime("1 seconds"))
-      .start()
-    */
     val query = joinStaticAndStream.writeStream.
       foreachBatch{ (batchDF: DataFrame, batchId: Long) =>
         batchDF.write.mode("append").jdbc(url, table, db_props )
-        //parquet(hdfs_path + "/user/results")
-        //jdbc(url, table, db_props )
       }
       .trigger(Trigger.ProcessingTime("1 seconds"))
       .start()
 
     query.awaitTermination()
-      /*
-      .withColumn("window_start", $"window.start")
-      .withColumn("window_end", $"window.end")
-      .drop($"window")
-      .writeStream.
-      foreachBatch{ (batchDF: DataFrame, batchId: Long) =>
-        batchDF.write.mode("append").jdbc(url, table, db_props )
-          //parquet(hdfs_path + "/user/results")
-        //jdbc(url, table, db_props )
-      }
-      .outputMode("complete")
-      .start()
-*/
-    //format("console").
-      //trigger(Trigger.ProcessingTime("30 seconds")).
-      //start()
-
-
-
-
-
-    /*
-    val kafkaLines = lines.selectExpr("CAST(value AS STRING)", "timestamp").
-      as[(String, Timestamp)]
-
-    val departmentTraffic = kafkaLines.
-      where(split(split($"value", " ")(6), "/")(1) === "department").
-      select(split(split($"value", " ")(6), "/")(2).alias("department_name"), $"timestamp").
-      groupBy(
-        window($"timestamp", "20 seconds", "20 seconds"),$"department_name"
-      ).
-      count()
-
-    val query = departmentTraffic.
-      writeStream.
-      outputMode("update").
-      format("console").
-      trigger(Trigger.ProcessingTime("20 seconds")).
-      start()
-
-    query.awaitTermination()
-    */
   }
 }
