247611a2cde3e3cdc52b60fb3b7573bacfaf55fd
AR-code <AR-code-99@protonmail.com>
2020-03-06 21:03:23 -0800

    Using ENV variables for kafka-streaming

--- kafka-streaming/build.sbt
+++ kafka-streaming/build.sbt
@@ -5,6 +5,4 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "1.0.0"
-libraryDependencies += "com.typesafe" % "config" % "1.3.2"
-libraryDependencies += "com.maxmind.geoip2" % "geoip2" % "2.12.0"
 
--- kafka-streaming/src/main/scala/KafkaProducerExample.scala
+++ kafka-streaming/src/main/scala/KafkaProducerExample.scala
@@ -1,25 +1,23 @@
 import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer,ProducerConfig,ProducerRecord}
-import com.typesafe.config.ConfigFactory
 
 object KafkaProducerExample {
   def main(args: Array[String]): Unit = {
-    val conf = ConfigFactory.load()
-    // first arg prod(AWS) or dev(local)
-    val envProps = conf.getConfig(args(0))
     // second arg filename to process
-    val fileName = args(1)
+    val fileName = args(0)
     println(fileName)
+    val bootstrap_server = sys.env("BOOTSTRAP_SERVER")
+    val batch_size = "16384"
+    val kafka_topic = "edgar-logs"
 
     val lineIter = io.Source.fromFile(fileName).getLines()
 
-    println("zookeeper: " + envProps.getString("zookeeper"))
-    println("bootstrap-server: " + envProps.getString("bootstrap.server"))
+    println("bootstrap-server: " + bootstrap_server)
 
     val props = new Properties
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, envProps.getString( "bootstrap.server"))
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap_server)
     props.put(ProducerConfig.CLIENT_ID_CONFIG,"KafkaProducerExample")
-    props.put(ProducerConfig.BATCH_SIZE_CONFIG, envProps.getString( "batch.size"))
+    props.put(ProducerConfig.BATCH_SIZE_CONFIG, batch_size)
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 
@@ -27,11 +25,9 @@ object KafkaProducerExample {
     val producer = new KafkaProducer[String, String](props)
 
     while (lineIter.hasNext) {
-      //Thread.sleep(1)
       val msg = lineIter.next()
       val ipAddr = msg.split(",")(0)
-      //println(msg)
-      val record = new ProducerRecord[String, String]("edgar-logs" , ipAddr, msg)
+      val record = new ProducerRecord[String, String](kafka_topic, ipAddr, msg)
       producer.send(record)
     }
 
